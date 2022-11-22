4abbf3384fe0277b954c26ac4d792fc4882fad74
Carla Willmes <willmes.carla@gmail.com>
2022-11-22 16:08:13 +0900

    refactored navbar code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-      <div className="nav">
+      <nav className="nav">
           <div className="navbar-left">
               <img className="logo-sm" src="./src/images/reactjs-icon.png"/>
               <h4>ReactFacts</h4>
           </div>
           <p>React Course – Project 1</p>
-      </div>
+      </nav>
   )
 }
