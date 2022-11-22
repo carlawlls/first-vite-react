ce1a5b8c324a1efe6cfbf91a1457ff4bf8683c2a
Carla Willmes <willmes.carla@gmail.com>
2022-11-22 17:17:31 +0900

    cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,6 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    /*
-    <div className="App">
-      <header className="App-header">
-        < Navbar />
-        <h1>Hello</h1>
-        <img src={pizza} className="pizza" alt="pizza" />
-        <p>Have a pizza</p>
-      </header>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      < Main />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-    */
     <>
       <Navbar />
       <Main />
